fb1fcd7343692bdcca9c1e1ed3d1318a12d927f4
Blake Meyer <blake.ynwa@gmail.com>
2018-08-30 18:33:25 -0700

    Add teams and leagues model, start creating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+pot_one = Team.create([{ name: 'Real Madrid' }, { name: 'Atletico Madrid' }, { name: 'Barcelona' }, { name: 'Bayern Munich' }, { name: 'Manchester City' }, { name: 'Juventus' }, { name: 'Paris Saint Germain' }, { name: 'Lokomotiv Moscow' }])
+pot_two = Team.create([{ name: 'Benfica' }, { name: 'Dortmund' }, { name: 'Manchester United' }, { name: 'Napoli' }, { name: 'Porto' }, { name: 'Roma' }, { name: 'Shakhtar Donestk' }, { name: 'Tottenham Hotspur' }])
+pot_three = Team.create([{ name: 'Dinamo Kiev' }, { name: 'PSV' }, { name: 'Liverpool FC' }, { name: 'Lyon' }, { name: 'AS Monaco' }, { name: 'CSKA Moscow' }, { name: 'Schalke' }, { name: 'Valencia' }])
+pot_four = Team.create([{ name: 'AEK Athens' }, { name: 'Club Brugge' }, { name: 'Galatasary' }, { name: 'Hoffenheim' }, { name: 'Inter Milan' }, { name: 'Red Star Belgrade' }, { name: 'Viktoria Plzen' }, { name: 'Young Boys' }])
